458e1286118736e6d0345b9cb135af8a641d12b0
R4GHAV <67642579+R4GHAV@users.noreply.github.com>
2021-01-23 22:27:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("hello")
+let text = document.querySelector('.text');
+let smallText =document.querySelector('.smallText');
+let addBtn = document.querySelector('.addBtn');
+addBtn.addEventListener('click', function () {
+  smallText.value = text.value;
+  localStorage.setItem('Note', text.value);
+  text.value='';
+});
+
