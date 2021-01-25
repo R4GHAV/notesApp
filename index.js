5789232192648aace78b1a805a033600423b5e70
R4GHAV <67642579+R4GHAV@users.noreply.github.com>
2021-01-25 21:33:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 let text = document.querySelector('.text');
 let count =0;
 let addBtn = document.querySelector('.addBtn');
-
 let yourNotes = document.querySelector('.yourNotes');
 
 addBtn.addEventListener('click', function () {
 
   if (text.value !='') {
     count+=1;
-    
-    yourNotes.innerHTML+=`<div class="an allNotes${count}"><h3><b>NOTE</b></h3><textarea name="name" class="smallText${count}" rows="8" cols="8" readonly></textarea><button type="button"  onClick='delfun(${count})' class= "del${count}"name="button">Delete note</button></div>`
+    var heading=document.querySelector('.head');
+    yourNotes.innerHTML+=`<div class="an allNotes${count}"><h3><b>${heading.value}</b></h3><textarea name="name" class="smallText${count}" rows="8" cols="8" readonly></textarea><button type="button"  onClick='delfun(${count})' class= "del${count}"name="button">Delete note</button></div>`
     localStorage.setItem('Note', text.value);
     filling();
   }
-
+  heading.value='';
   text.value='';
+
 });
 
 function filling() {
   let smallText =document.querySelector(`.smallText${count}`);
-  
   var textnode = document.createTextNode(text.value);
   smallText.appendChild(textnode);
 };
